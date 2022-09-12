45e9ee76d4ea1ca65dc4c6f3771ea087b3147afa
divyanshu0x16 <divyanshu22.dm@gmail.com>
2022-09-12 23:20:54 +0530

    completed ls and cwd commands

--- server/fileService.py
+++ server/fileService.py
@@ -1,12 +1,15 @@
 import os
+import subprocess
 
 def handleCommand(command):
     #TODO: Complete Functionality
 
     if (command == 'cwd'):
-        return command
+        output = subprocess.run('pwd', capture_output=True).stdout.decode('utf-8')
+        return output
     elif (command == 'ls'):
-        return ls
+        output = subprocess.run(command, capture_output=True).stdout.decode('utf-8')
+        return output
     elif ( command.startswith('cd ') ):
         #checking if the path is a directory
         file_path = command[3:]
